fccb217af695835377bd9b6c3a88232c9ab0375f
MikeMoolenaar <heeylego@gmail.com>
2022-12-22 20:41:12 +0100

    Finished day 8 part 1

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -2,45 +2,46 @@
 
 var lines = content.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
 
-int alwaysVisible = lines[0].Length*2 + (lines.Count - 2)*2;
-
 string Reverse(string s) => new(s.Reverse().ToArray());
 
+int alwaysVisible = lines[0].Length*2 + (lines.Count - 2)*2;
+
 List <(int, int)> seenCoords = new();
-void checkIfCanSee(string trees, (int,int) coords)
+void CheckIfCanSee(string trees, (int,int) coords)
 {
-    trees = trees.Replace("0", "");
     int currentTree = Convert.ToInt32(lines[coords.Item1][coords.Item2].ToString());
-    if (currentTree == 0) return;
     var lsInt = trees.Select(x => Convert.ToInt32(x.ToString())).ToList();
-    var lsIntSorted = lsInt.OrderDescending();
-    if (lsInt.Count == 0 ||
-        (lsInt.SequenceEqual(lsIntSorted) && lsInt.First() < currentTree))
+    if (lsInt.All(x => x < currentTree))
         seenCoords.Add(coords);
 }
-
 
 for (int rowi = 1; rowi < lines.Count - 1; rowi++)
 {
     for (int coli = 1; coli < lines[0].Length - 1; coli++)
     {
         // Left - right & right - left
-        checkIfCanSee(lines[rowi][..(coli)], (rowi,coli));
-        checkIfCanSee(lines[rowi][(coli+1)..], (rowi,coli));
+        CheckIfCanSee(lines[rowi][..coli], (rowi,coli));
+        CheckIfCanSee(Reverse(lines[rowi][(coli + 1)..]), (rowi,coli));
         // Top - bottom & bottom - top
         string completeCOl = string.Empty;
         foreach (var t in lines)
             completeCOl += t[coli];
 
-        checkIfCanSee(completeCOl[..(rowi)], (rowi,coli));
-        checkIfCanSee(completeCOl[(rowi+1)..], (rowi,coli));
+        CheckIfCanSee(completeCOl[..rowi], (rowi,coli));
+        CheckIfCanSee(Reverse(completeCOl[(rowi + 1)..]), (rowi,coli));
     }
 }
 
-int internalVisible = seenCoords.Select(x => $"{x.Item1}{x.Item2}")
-    .GroupBy(x => x)
-    .Count();
+var internalVisibleList = seenCoords.Select(x => $"{x.Item1} {x.Item2}")
+    .Distinct()
+    .ToList();
+
+foreach (var s in internalVisibleList)
+{
+    var coords = (Convert.ToInt32(s.Split(' ')[0]), Convert.ToInt32(s.Split(' ')[1]));
+    Console.WriteLine($"{s} {lines[coords.Item1][coords.Item2]}");
+}
 
 Console.WriteLine();
-Console.WriteLine($"{alwaysVisible} + {internalVisible}");
-Console.WriteLine(alwaysVisible + internalVisible);
+Console.WriteLine($"{alwaysVisible} + {internalVisibleList.Count}");
+Console.WriteLine(alwaysVisible + internalVisibleList.Count);
