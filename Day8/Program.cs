d3f0b3843d71ddb2d15f8ebafc7f9672697a5b22
MikeMoolenaar <heeylego@gmail.com>
2022-12-22 20:43:13 +0100

    Improve formatting

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -2,8 +2,6 @@
 
 var lines = content.Split('\n').Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
 
-string Reverse(string s) => new(s.Reverse().ToArray());
-
 int alwaysVisible = lines[0].Length*2 + (lines.Count - 2)*2;
 
 List <(int, int)> seenCoords = new();
@@ -21,14 +19,14 @@ for (int rowi = 1; rowi < lines.Count - 1; rowi++)
     {
         // Left - right & right - left
         CheckIfCanSee(lines[rowi][..coli], (rowi,coli));
-        CheckIfCanSee(Reverse(lines[rowi][(coli + 1)..]), (rowi,coli));
+        CheckIfCanSee(lines[rowi][(coli + 1)..], (rowi,coli));
         // Top - bottom & bottom - top
         string completeCOl = string.Empty;
         foreach (var t in lines)
             completeCOl += t[coli];
 
         CheckIfCanSee(completeCOl[..rowi], (rowi,coli));
-        CheckIfCanSee(Reverse(completeCOl[(rowi + 1)..]), (rowi,coli));
+        CheckIfCanSee(completeCOl[(rowi + 1)..], (rowi,coli));
     }
 }
 
@@ -36,6 +34,7 @@ var internalVisibleList = seenCoords.Select(x => $"{x.Item1} {x.Item2}")
     .Distinct()
     .ToList();
 
+Console.WriteLine("Found positions (x y val)");
 foreach (var s in internalVisibleList)
 {
     var coords = (Convert.ToInt32(s.Split(' ')[0]), Convert.ToInt32(s.Split(' ')[1]));
@@ -43,5 +42,5 @@ foreach (var s in internalVisibleList)
 }
 
 Console.WriteLine();
-Console.WriteLine($"{alwaysVisible} + {internalVisibleList.Count}");
-Console.WriteLine(alwaysVisible + internalVisibleList.Count);
+Console.WriteLine($"{nameof(alwaysVisible)} + {nameof(internalVisibleList.Count)}");
+Console.WriteLine($"{alwaysVisible} + {internalVisibleList.Count} = {alwaysVisible + internalVisibleList.Count}");
